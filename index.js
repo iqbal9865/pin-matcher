9e8e3865049195ed33f68a0e3d71a2c1d5d13278
Iqbal Ahmed <iqbalhussain9865@gmail.com>
2021-01-31 22:49:47 +0600

    not complete yet

--- index.js
+++ index.js
@@ -12,3 +12,34 @@ function getPin(){
 function generatePin(){
     var pinInput = document.getElementById('pin-generate-input').value = getPin();
 }
+
+//handle calculator button event
+document.getElementById('digits-container').addEventListener('click',function(event){
+    const digit = event.target.innerText;
+    if(isNaN(digit)){
+        //handle backspace
+        //handle clear
+        if(digit === 'C'){
+            const inputDigit = document.getElementById('typed-pin');
+            inputDigit.value = '';
+        }
+
+    }
+    else{
+        const inputDigit = document.getElementById('typed-pin');
+        inputDigit.value = inputDigit.value + digit; 
+    }
+});
+function verifyPin(){
+    var pin = document.getElementById('pin-generate-input').value
+    const inputDigit = document.getElementById('typed-pin').value;
+    if(pin === inputDigit){
+        document.getElementById('match').style.display = "block";
+        document.getElementById('not-match').style.display = "none";
+    }
+    else{
+        document.getElementById('not-match').style.display = 'block';
+        document.getElementById('match').style.display = "none";
+    }
+    
+}
